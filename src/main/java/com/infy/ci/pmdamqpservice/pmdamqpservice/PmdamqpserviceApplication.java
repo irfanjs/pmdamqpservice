797eef1b389fd49fbdd39a49e4303c21bc219f09
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-07 15:22:20 +0530

    code modify

---
+++ src/main/java/com/infy/ci/pmdamqpservice/pmdamqpservice/PmdamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.pmdamqpservice.pmdamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdamqpserviceApplication.class, args);
+	}
+
+}
